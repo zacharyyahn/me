be8580954d37af66c30ab0ae1171eacf947562ef
zacharyyahn <zry2yz@virginia.edu>
2021-06-16 11:46:33 -0400

    Added about me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Hello from './sayHello';
 import './App.css';
+import PropagateLoader from "react-spinners/PropagateLoader";
+import github from './github.png';
+import linkedin from './linkedin.jpg';
 
 function App() { //just a normal JS function
 
@@ -12,9 +15,23 @@ function App() { //just a normal JS function
   //   setRed(!isRed);
   // }
 
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2500)
+  }, [])
+
   return(
     <div className="app">
-      
+      {
+        loading ?
+        <div className="spinner">
+        <PropagateLoader color={"#7F2BB9"} loading={loading} size={15} />
+        </div>
+        :
       <div className="app-wrapper">
           
         <div className="statement-wrapper">
@@ -30,13 +47,36 @@ function App() { //just a normal JS function
           </div>
 
         </div>
+
+        <div className="about-me-wrapper">
+          <div className="about-me-text">
+            <div className="about-me-text-title">
+            <h2> About Me </h2>
+            </div>
+            <div className="about-me-text-content">
+              <p style={{marginBottom: 1.25 + 'em'}}>3rd-year undergraduate at UVA</p>
+              <p style={{marginBottom: 1.25 + 'em'}}>Pursuing degrees in Computer Science and Computer Engineering</p>
+              <p> I value enthusiasm, curiosity, persistence, and doing things the right way</p>
+            </div>
+            <div className="about-me-github-image">
+              <a href="https://github.com/zacharyyahn">
+                <img src={github} width={60} height={60}></img>
+              </a>
+            </div>
+          <div className="about-me-linkedin-image">
+            <a href="https://www.linkedin.com/in/zachary-yahn-87a431199/">
+              <img src={linkedin} width={60} height={60}></img>
+            </a>
+          </div>
+          </div>
+          <div className="about-me-image"></div>
+
+      
+        </div>
         
         <div className="projects-wrapper">
         </div>
 
-        <div className="about-me-wrapper">
-      
-        </div>
         {/* <button onClick={increment}>Increment</button>
         <h1>{count}</h1>
         <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
@@ -44,6 +84,7 @@ function App() { //just a normal JS function
         <Tweet name="Peasant" message="I'm bad"/>
         <Tweet name="Test" message="123"/> */}
         </div>
+      }     
     </div>
   );
 }
---
+++ src/LoadSpinner/LoadSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LoadSpinner.css';
+
+const LoadSpinner = () => (
+    <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+);
+
+export default LoadSpinner;
